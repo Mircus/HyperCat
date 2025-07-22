f6fcb1c6cdd54f26e0f9e5a04f3f88a2763f4324
mirco mannucci <mirco@holomathics.com>
2025-07-22 14:44:51 -0400

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,1 @@
-from .core import Object, Morphism
-from .category import Category
-from .functor import Functor
-from .natural import NaturalTransformation
-from .twocategory import TwoCell, TwoCategory
-from .standard import StandardCategories
-from .example import example_usage
+
